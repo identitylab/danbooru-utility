a37a0eb04cc117f3e525738bc9f186630dd952a5
Xingbo <xingbod@gmail.com>
2020-11-11 13:01:43 +0800

    update from Xingbo

--- danbooru/danbooru_utility.py
+++ danbooru/danbooru_utility.py
@@ -358,7 +358,7 @@ def resize_and_save_image(load_path, write_file, save_dir, link_dir, img_size, o
     img = resizeimage.resize_contain(img, [img_size, img_size])
     print('Saving!!!!')
     rgb_im = img.convert('RGB')
-    rgb_im.save(write_path, rgb_im.format)
+    rgb_im.save(write_path, img.format)
     rgb_im.close()
     return 1
 
