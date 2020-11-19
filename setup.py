2716e24c68a14c519c8a9cf2610076e3809a36e1
charleslucky <xingbod@gmail.com>
2020-11-19 13:47:35 +0800

    Json process

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = 'danbooru-utility',
-    version = '0.1.25',
+    version = '0.2.0',
     url = 'https://github.com/identitylab/danbooru-utility.git',
     author = 'Reid Sanders',
     author_email = 'reid@reidsanders.net',
